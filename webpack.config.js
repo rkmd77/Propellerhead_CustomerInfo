75822cd4dc0e7c50ee51829601fc2c57978fe5b1
rkmd77 <rochenz@outlook.com>
2018-08-16 13:28:17 +1200

    initial project

---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const proxy = require('http-proxy-middleware')
+
+const context = [`/suburbs`, `/materials`, `/colours`, `/data`]
+
+module.exports = {
+  entry: './src/app.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: 'js/bundle.js'
+  },
+  resolve: {
+    alias: {
+      pages: path.resolve(__dirname, 'src/pages'),
+      component: path.resolve(__dirname, 'src/component'),
+      util: path.resolve(__dirname, 'src/util'),
+      service: path.resolve(__dirname, 'src/service')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react']
+          }
+        }
+      },
+      // css文件的处理
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      },
+      // sass文件的处理
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name: 'resource/[name].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      favicon: './favicon.ico'
+    }),
+    new ExtractTextPlugin("css/[name].css"),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'common',
+      filename: 'js/base.js'
+    })
+  ],
+  devServer: {
+    host: 'localhost',
+    port: '8081',
+    historyApiFallback: {
+      index: '/dist/index.html'
+    },
+    proxy: [{
+      context: context,
+      target: 'http://localhost:8181',
+      changeOrigin: true
+    }]
+  }
+};
